7f79a08134ced1afab131a042badb477592afbfb
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-10-04 15:54:55 -0400

    collision method working,sends alert. very rough draft

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const morty = document.getElementById("figure");
+const portal = document.getElementById("obstacle")
 
 function jump() {
   if (morty.classList != "jump") {
@@ -9,6 +10,23 @@ function jump() {
     }, 300)
   }
 }
+
+let isAlive = setInterval(() => {
+ // get current morty Y position
+  let mortyTop = parseInt(window.getComputedStyle(morty).getPropertyValue("top"));
+
+  //get current portal y position
+  let portalLeft = parseInt(window.getComputedStyle(portal).getPropertyValue("left"))
+
+// detect collision
+  
+  if (portalLeft < 50 && portalLeft > 0 && mortyTop >= 140) {
+    console.log('collision')
+    alert("Wubba Lubba Dub Dub, YOU LOSE!")
+  }
+  
+}, 10);
+
 
 document.addEventListener("keydown", function (event) {
   jump();
