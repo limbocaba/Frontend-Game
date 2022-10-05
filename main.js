dfb80fda5e136fc574c93cb1d832fd40b6b10fdc
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-10-05 14:47:31 -0400

    added an if statement for ship collision, not working that well

--- main.js
+++ main.js
@@ -1,32 +1,42 @@
 const morty = document.getElementById("figure");
-const portal = document.getElementById("obstacle")
+const portal = document.getElementById("obstacle");
+const ship = document.getElementById("ship");
 
 function jump() {
   if (morty.classList != "jump") {
     morty.classList.add("jump");
 
     setTimeout(function () {
-      morty.classList.remove("jump")
-    }, 400)
+      morty.classList.remove("jump");
+    }, 400);
   }
 }
 
 let isAlive = setInterval(() => {
- // get current morty Y position
-  let mortyTop = parseInt(window.getComputedStyle(morty).getPropertyValue("top"));
+  // get current morty Y position
+  let mortyTop = parseInt(
+    window.getComputedStyle(morty).getPropertyValue("top")
+  );
 
   //get current portal y position
-  let portalLeft = parseInt(window.getComputedStyle(portal).getPropertyValue("left"))
+  let portalLeft = parseInt(
+    window.getComputedStyle(portal).getPropertyValue("left")
+  );
 
-// detect collision
-  
+  let shipLeft = parseInt(
+    window.getComputedStyle(ship).getPropertyValue("left")
+  );
+  // detect collision
+
   if (portalLeft < 50 && portalLeft > 0 && mortyTop >= 140) {
-    console.log('collision')
-    alert("Wubba Lubba Dub Dub, YOU LOSE!")
+    console.log("collision");
+    alert("Wubba Lubba Dub Dub, YOU LOSE!");
   }
-  
+  // detect ship collision
+  if (shipLeft < 50 && shipLeft > 0 && mortyTop == 120) {
+    alert("Duck next time. You LOSE!");
+  }
 }, 10);
-
 
 document.addEventListener("keydown", function (event) {
   jump();
